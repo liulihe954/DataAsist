2dc02ae331d00ae6ac1e8b79a37f6ca6b340af58
Lihe Liu <lihe.liu@ufl.edu>
2020-03-17 20:35:30 -0400

    up

--- ESBL_Dynamics_Matrix/CorTest.R
+++ ESBL_Dynamics_Matrix/CorTest.R
@@ -1,12 +1,26 @@
-library(Hmisc)
+library(Hmisc);library(tidyverse)
+options(stringsAsFactors = FALSE) # prevent converting to factors
 dat <- read.csv('0-12M_gene-Month_correlation.csv')
-corr_list <- list()
-for (i in 2:nrow(dat)){
-  r <- cor.test(unlist(dat[1,],use.names = F),
-                unlist(dat[i,],use.names = F),
-                method='pearson')
-  corr_list[[paste("Genes 1 &", i)]] <- c(r$estimate, p.val=r$p.value)
-}
-length(corr_list)
-write.table(corr_list, "cor_out.txt", sep="\t")
 
+# set a baseline to compare with and set a index for all gene names
+Month = seq(0,12,by = 3);all_gene = dat[,1] 
+# set a container for output
+out = data.frame()
+
+# loop around each gene
+for (i in seq_along(rownames(dat))){
+  tmp = as.numeric(dat[i,2:6])
+  r <- cor.test(Month,tmp,method='pearson')
+  out = rbind(out, c(all_gene[i],r$estimate,r$p.value))
+}
+colnames(out) = c('Gene','Cor','pval')
+
+# filter out less informative ones
+thres1 = 0.8  # subject to change
+pval_thres = 0.05  # subject to change
+out = out %>% dplyr::filter(Cor != 'NA',pval !='NA') %>% 
+  dplyr::filter(Cor > thres1,
+                pval< pval_thres)
+
+# output format
+write.table(out, "CorOut_0.8_0.05.txt", sep="\t")
