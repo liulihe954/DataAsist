be6c3d1c0463dd2e1958c3122fefae255ada7d0d
Lihe Liu <lihe.liu@ufl.edu>
2020-03-17 20:39:35 -0400

    up

--- ESBL_Dynamics_Matrix/CorTest.R
+++ ESBL_Dynamics_Matrix/CorTest.R
@@ -18,9 +18,9 @@ colnames(out) = c('Gene','Cor','pval')
 # filter out less informative ones
 thres1 = 0.8  # subject to change
 pval_thres = 0.05  # subject to change
-out = out %>% dplyr::filter(Cor != 'NA',pval !='NA') %>% 
-  dplyr::filter(Cor > thres1,
-                pval< pval_thres)
+out_final = out %>% dplyr::filter(Cor != 'NA',pval !='NA') %>% 
+  dplyr::filter(Cor > thres1,pval < pval_thres)
 
 # output format
-write.table(out, "CorOut_0.8_0.05.txt", sep="\t")
+write.table(out, "CorOut_all.txt", sep="\t")
+write.table(out_final, "CorOut_0.8_0.05.txt", sep="\t")
