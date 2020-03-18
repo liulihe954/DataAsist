6983d621b686b60ba40c861868260bbcfdb8fb1f
Lihe Liu <lihe.liu@ufl.edu>
2020-03-17 20:52:29 -0400

    up

--- ESBL_Dynamics_Matrix/CorTest.R
+++ ESBL_Dynamics_Matrix/CorTest.R
@@ -18,8 +18,13 @@ colnames(out) = c('Gene','Cor','pval')
 # filter out less informative ones
 thres1 = 0.8  # subject to change
 pval_thres = 0.05  # subject to change
-out_final = out %>% dplyr::filter(Cor != 'NA',pval !='NA') %>% 
-  dplyr::filter(Cor > thres1,pval < pval_thres)
+qval_thres = 0.1 # subject to change
+
+out_final = out %>% 
+  #mutate(qval = p.adjust(Cor,'fdr')) %>% # maybe later you need to use qvalue
+  dplyr::filter(Cor != 'NA',pval !='NA') %>% 
+  dplyr::filter(Cor > thres1,
+                pval < pval_thres)
 
 # output format
 write.csv(out, "CorOut_all.csv",row.names = F,quote = F)
